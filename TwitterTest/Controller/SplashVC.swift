f4603e370dc05db18af53803e1434b63115a1fba
keba76 <Harman@192.168.0.101.local.ipnet.ua>
2017-05-10 11:30:44 +0300

    MMVM

--- TwitterTest/Controller/SplashVC.swift
+++ TwitterTest/Controller/SplashVC.swift
@@ -79,6 +79,8 @@ class SplashVC: UIViewController, SegueHandlerType {
         }
     }
     
+    
+    
     func alert(title: String, message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
